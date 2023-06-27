55c8a0bd623edf8a8b3c182c09262f4512b0c7b2
amograo24 <amograo24@gmail.com>
2023-06-27 16:57:31 +0530

    good no of questions done

--- Ch5_Repetition/notes/forloop.cpp
+++ Ch5_Repetition/notes/forloop.cpp
@@ -36,15 +36,59 @@ int main() {
     cout << "\n\n";
     cout << "i1: " << (i1=5) << endl;
     // check from here, if i can define multiple variables of same type? multiple variables of different types?
-    for(int i2=1,i3=4;i1;cout << endl) { 
-        cout << i2 << " " << i1;
-        i1--;
-        i2*=2;
+    for(int i2=1,i3=4;i3;i3--,i2--) { 
+        cout << i2 << " " << i3 << endl;
     }
+    cout << "\n\n";
+    // for((int i2=1,i3=4, float f1=33.4);i3;i3--,i2--) {  // not valid as it whie initializing, it expects only ints. So it's like trying int float m;
+    int i2, i3; float f1;   
+    for(i2=1,i3=4,f1=33.4;i3;i3--,i2--,f1+=2.3) {
+        cout << i2 << " " << i3 << " " << f1 << endl;
+    }
+    cout << i2 << " " << i3 << " " << f1 << endl;
+    cout << "\n\n";
+    cout << "\n testing scope now\n";
+    {
+        int sci2, sci3; float scf1;   
+        for(sci2=1,sci3=4,scf1=33.4;sci3;sci3--,sci2--,scf1+=2.3) {
+            cout << sci2 << " " << sci3 << " " << scf1 << endl;
+        } 
+        cout << sci2 << " " << sci3 << " " << scf1 << endl;
+    }
+    // cout << sci2 << " " << sci3 << " " << scf1 << endl; // error, as the variables are not defined in this scope
 
     // for(;1<2;cout << "Hello\n"); // no error, but results in an infinite loop
 
+    for(int z=0;!z;z++){
+        // int z=5; // invalid as it is redefining z which is already declared for this for loop scope
+    }
+
+    // {int m;}
+    // m=6; // error, as m is not defined in this scope
+
+    for(int xx=0;xx<5;xx++) {
+        {int m;}
+        // cout << m; // error, as m is not defined in this scope
+        cout << xx << " ";
+    }
+
+    int kk;
+    {kk=5;} // valid, as kk is defined in this scope
+    int mm=5;
+    {int mm=8; cout << endl << mm << endl;} // valid, as mm is defined in this scope
+    cout << mm << endl;
+
     // while(cout << "\nHello\n"); // no error, but results in an infinite loop
     // cout << "Checking if the loop has been exited.\n";
+
+    /* defining a variable in do, and checking if it will work in while */
+    // do {
+    //     int di1=5;
+    //     di1--;
+    // } while(di1);
+
+    /* condition in for loop */
+    // for(int i=0;i<5;if(kk) i++) {
+    // }
     return 0;
 }
