4c74da69459b25d248397d7c5ee04c567415027d
amograo24 <amograo24@gmail.com>
2023-06-29 17:37:13 +0530

    done 1 more prob

--- Ch5_Repetition/programs/p8.cpp
+++ Ch5_Repetition/programs/p8.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main() {
 
+    int inp;
+    do {
+        cout << "Enter a +ve integer: ";
+        cin >> inp;
+    } while(inp<=0);
     
+    for(int i=1;i<=inp/2;i++){
+        if(inp%i==0) cout << i << " ";
+    }
+    cout << endl;
+    for(int i=inp/2;i;i--){
+        if(!(inp%i)) cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
